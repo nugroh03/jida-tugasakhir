362b323d4ece82e306ba4f17878856afefdb6946
nugroh03 <nugroho.dwi.cahyono@gmail.com>
2025-07-31 23:52:17 +0700

    feat : fix build-7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}", // scan file di dalam src/
   ],
   theme: {
     extend: {},
